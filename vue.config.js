5600bfa1ee8cdda9f60153491d093ca605c158d3
Hitesh Kumar <hitesh.kumar399@gmail.com>
2019-12-16 23:33:13 +0530

    fixed config

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,8 @@ module.exports = {
             'vue': 'vue',
             'lq-form': 'lq-form',
             'date-fns': 'date-fns',
-            'lodash': 'lodash',
+            'lodash/core': 'lodash/core',
+            'lodash/cloneDeep': 'lodash/cloneDeep',
         }
         config.externals = {
             ...config.externals,
